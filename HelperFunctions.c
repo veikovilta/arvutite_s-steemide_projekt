84a0c600fb36bcd6324fa86ede6c192851190ae8
Kaur Andro Orgusaar <kaurandro2012@gmail.com>
2024-10-03 14:43:17 +0300

    added includes

--- HelperFunctions.c
+++ HelperFunctions.c
@@ -1,4 +1,11 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include "stdbool.h"
+#include "LedBlink.h"
 
 void preciseSleep(int seconds) {
     struct timespec req, rem;
